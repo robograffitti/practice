f817722b7153d197c4312da7d6157b691de13d3b
Yoshimaru Tanaka <y-tanaka@Penta-W520.(none)>
2014-06-29 00:10:07 +0900

    Implemented recursive algorithm.

--- cpp/test000.cpp
+++ cpp/test000.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 using namespace std; // std namespace is used
 
+
+void RecursiveCall(int argc, char *argv[]); // prototype declaration
+
 int main(int argc, char *argv[]) {
   int month = 11, day = 3;
   double percent = 25.2525;
 
-  cout << "argc:" << argc << "\n";
+  /* cout << "argc:" << argc << "\n";
   for (int i=0; i < 2; i++) {
     cout << "argv[" << i << "]:" << argv[i] << "\n";
-  }
+    } */
+  // Reprement with recursive call
+  RecursiveCall(argc, argv);
 
   cout << month << "月";
   cout << day << "日\n";
   cout << "降水確率" << percent << "%\n";
 
   return 0;
+}
+
+void RecursiveCall(int argc, char *argv[]) {
+  argc -= 1;
+  if (argc >= 0) {
+    cout << "argv[" << argc  << "]:" << argv[argc] << "\n";
+  } else {
+    return;
+  }
+  return RecursiveCall(argc, argv);
 }
