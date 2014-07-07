69dd8693bbd30810daf40533f25c438dcb6c9202
Yoshimaru Tanaka <y-tanaka@Penta-W520.(none)>
2014-07-07 09:36:58 +0900

    Moved variabl declaration to bottom.

--- cpp/test000.cpp
+++ cpp/test000.cpp
@@ -5,16 +5,15 @@ using namespace std; // std namespace is used
 void RecursiveCall(int argc, char *argv[]); // prototype declaration
 
 int main(int argc, char *argv[]) {
-  int month = 11, day = 3;
-  double percent = 25.2525;
-
   /* cout << "argc:" << argc << "\n";
   for (int i=0; i < 2; i++) {
     cout << "argv[" << i << "]:" << argv[i] << "\n";
     } */
   // Reprement with recursive call
   RecursiveCall(argc, argv);
-
+  
+  int month = 11, day = 3;
+  double percent = 25.2525;
   cout << month << "月";
   cout << day << "日\n";
   cout << "降水確率" << percent << "%\n";
